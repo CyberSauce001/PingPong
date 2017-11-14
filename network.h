656774eeb3e3b93c1337082e0ac1df3e2f8daba9
Ethereal15 <xionnguyen@gmail.com>
2017-11-13 20:48:09 -0800

    creating a network connection

--- network.h
+++ network.h
@@ -1,1 +1,15 @@
 #include <SDL/SDL_net.h>
+#include <iostream>
+#include <cstring>
+
+class network{
+  SDLNet_SocketSet server;
+  TCPsocket connection;
+  char tmp[1400];
+  public:
+    network(const char* ip);
+    ~network();
+    void send(player* p);
+    void sendHit(player* p,int id);
+    void recv(std::vector<player*>& players, std::vector::<paddle*> paddle, player* p, std::vector<unsigned int> f);
+}
